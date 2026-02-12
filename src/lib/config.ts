e155497eaefdbab7db00b38cd7c84255b986d484
x20080406 <x20080406@users.noreply.github.com>
2026-02-13 00:17:56 +0800

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -194,8 +194,7 @@ async function initConfig() {
           SiteConfig: {
             SiteName: process.env.SITE_NAME || 'MoonTV',
             Announcement:
-              process.env.ANNOUNCEMENT ||
-              '本网站仅提供影视信息搜索服务，所有内容均来自第三方网站。本站不存储任何视频资源，不对任何内容的准确性、合法性、完整性负责。',
+              process.env.ANNOUNCEMENT || '',
             SearchDownstreamMaxPage:
               Number(process.env.NEXT_PUBLIC_SEARCH_MAX_PAGE) || 5,
             SiteInterfaceCacheTime: fileConfig.cache_time || 7200,
